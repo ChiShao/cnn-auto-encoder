c036396c896ecc531cc36802962558746620c785
Daniel Scholz <24234559+daniel-scholz@users.noreply.github.com>
2019-06-24 16:58:48 +0200

    Adds (bad) classifier for encoded representation

--- conv-autoencoder.py
+++ conv-autoencoder.py
@@ -1,32 +1,63 @@
 import os.path
+import sys
 
-import keras.models
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.datasets import mnist
-from keras.layers import Conv2D, Dense, Flatten, Input, MaxPooling2D, UpSampling2D
+from keras.layers import (
+    Conv2D, Dense, Flatten, Input, MaxPooling2D, Reshape, UpSampling2D)
+from keras.models import Model, Sequential, load_model
 from keras.utils import to_categorical
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from sklearn.metrics import confusion_matrix
 
 # this is the size of our encoded representations
 ENCODING_DIM = 10
+THRESHOLD = 0.5
+np.set_printoptions(threshold=sys.maxsize)
+
+CUR_DIR = os.path.curdir
 
 
 def get_data():
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    img = X_train[0].reshape((28, 28))
-    # reshape data to fit model
-    plt.imshow(X_train[0], cmap="Greys", vmin=0, vmax=255)
-    plt.savefig("example_img.png")
+    # X = np.concatenate( (X_train, X_test),axis=0)
+    # y = np.concatenate( (y_train, y_test),axis=0)
+    # print(X.shape, y.shape)
+
+    # print(data)
+    # np.random.shuffle(data)
+    # X = data[:,:-2]
+    # y = data[:,-2:]
 
     X_train = X_train.reshape((len(X_train), 28, 28, 1))
-    X_test = X_test.reshape((len(X_test), 28, 28, 1))
+    test_len = len(X_test)
+    X_test = X_test.reshape((test_len, 28, 28, 1))
     np.random.shuffle(X_test)
-    X_validate = X_test[len(X_test)//2:]
-    X_test = X_test[:len(X_test)//2]
+    X_validate = X_test[test_len//2:]
+    X_test = X_test[:test_len//2]
 
-    return X_train/255.0, X_test/255.0, X_validate/255.0
+    # one-hot encode target column
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    y_validate = y_test[test_len//2:]
+    y_test = y_test[:test_len//2]
+
+    return (X_train/255.0, X_test/255.0, X_validate/255.0), (y_train, y_test, y_validate)
+
+
+def build_classifier():
+    classifier = Sequential()
+    # classifier.add(Dense(16, activation="relu"))
+    classifier.add(Dense(10, activation="softmax", input_dim=128,
+                         kernel_initializer="random_normal"))
+
+    classifier.compile(optimizer='adam', loss='mean_squared_error',
+                       metrics=['accuracy'])
+    return classifier
+
+# TODO: test regularizing the model
 
 
 def build_conv_aue():
@@ -47,8 +78,13 @@ def build_conv_aue():
                     padding="same")(encode)
     # "encoded" is the encoded representation of the input, middle layer of the aue
     encoded = MaxPooling2D(
-        DEFAULT_STRIDE, name="encoder", padding="same")(encode)
+        DEFAULT_STRIDE, padding="same", name="encoder")(encode)
+    # encode = Flatten()(encode)
+    # # mitte
+    # encoded = Dense(10, name="encoder",  activation="relu")(encode)
 
+    # decode = Dense(4*4*8, activation="relu")(encoded)
+    # decode = Reshape((4, 4, 8))(decode)
     # layer between middle and output layer
     decode = Conv2D(8, DEFAULT_KERNEL, activation="relu", padding="same"
                     )(encoded)
@@ -62,37 +98,22 @@ def build_conv_aue():
                      padding="same")(decode)
 
     # this model maps an input to its reconstruction
-    autoencoder = keras.models.Model(inputs=input_img, outputs=decoded)
+    autoencoder = Model(inputs=input_img, outputs=decoded)
 
-    # this model maps an input to its encoded representation; Big image to small rep
-    encoder = keras.models.Model(
-        inputs=autoencoder.input, outputs=autoencoder.get_layer("encoder").output)
-
-    # create a placeholder for an encoded (ENCODING_DIM-dimensional) input
-    encoded_input = Input(shape=(4, 4, 8))
-    decoder = autoencoder.layers[-7](encoded_input)
-    decoder = autoencoder.layers[-6](decoder)
-    decoder = autoencoder.layers[-5](decoder)
-    decoder = autoencoder.layers[-4](decoder)
-    decoder = autoencoder.layers[-3](decoder)
-    decoder = autoencoder.layers[-2](decoder)
-    decoder = autoencoder.layers[-1](decoder)
-    # create the decoder model; small rep to big image
-    decoder = keras.models.Model(encoded_input, decoder)
-
+    encoder, decoder = get_codec_from_aue(autoencoder)
     # build (aka "compile") the model
     autoencoder.compile(optimizer="adadelta", loss="binary_crossentropy")
     return autoencoder, encoder, decoder
 
 
-def get_codec_from_aue(autoencoder):
+def get_codec_from_aue(autoencoder, mid_shape=(4, 4, 8)):
     # this model maps an input to its encoded representation; Big image to small rep
-    encoder = keras.models.Model(
+    encoder = Model(
         inputs=autoencoder.input, outputs=autoencoder.get_layer("encoder").output)
 
     # create a placeholder for an encoded (ENCODING_DIM-dimensional) input^
-    #TODO: remove hard coded shape
-    encoded_input = Input(shape=(4, 4, 8))
+    # TODO: remove hard coded shape
+    encoded_input = Input(shape=mid_shape)
     decoder = autoencoder.layers[-7](encoded_input)
     decoder = autoencoder.layers[-6](decoder)
     decoder = autoencoder.layers[-5](decoder)
@@ -101,41 +122,57 @@ def get_codec_from_aue(autoencoder):
     decoder = autoencoder.layers[-2](decoder)
     decoder = autoencoder.layers[-1](decoder)
     # create the decoder model; small rep to big image
-    decoder = keras.models.Model(encoded_input, decoder)
+    decoder = Model(encoded_input, decoder)
     return encoder, decoder
 
 
-X_train, X_test, X_validate = get_data()
-
-ckpt_loc = "ckpts/.conv-aue.hdf5"
+print("Getting data...")
+(X_train, X_test, X_validate), (y_train, y_test, y_validate) = get_data()
+print("Done")
+#####################################################################
+print("AUTOENCODER")
+#####################################################################
+ckpt_loc = os.path.join(CUR_DIR, "ckpts", ".conv-aue.hdf5")
 
 if(os.path.isfile(ckpt_loc)):
-    autoencoder = keras.models.load_model(ckpt_loc)
+    print("Loading Autoencoder from directory %s..." % ckpt_loc)
+    autoencoder = load_model(ckpt_loc)
     encoder, decoder = get_codec_from_aue(autoencoder)
+
 else:
+    print("Training Autoencoder...")
     autoencoder, encoder, decoder = build_conv_aue()
     earlyStopping = EarlyStopping(
-        monitor='val_loss', patience=10, verbose=0, mode='min')
+        monitor='val_loss', patience=10, verbose=1, mode='min', min_delta=0.01)
     mcp_save = ModelCheckpoint(ckpt_loc,
-                               save_best_only=True, monitor='val_loss', mode='min')
+                               save_best_only=True, verbose=1, monitor='val_loss', mode='min')
     reduce_lr_loss = ReduceLROnPlateau(
-        monitor='val_loss', factor=0.1, patience=3, verbose=1, epsilon=1e-4, mode='min')
+        monitor='val_loss', factor=0.3, patience=3, verbose=1, mode='min')
     autoencoder.fit(X_train, X_train,
-                    epochs=50,
+                    epochs=100,
                     batch_size=100,
                     shuffle=True,
                     validation_data=(X_validate, X_validate), callbacks=[earlyStopping, mcp_save, reduce_lr_loss])
+    model_path = os.path.join(CUR_DIR, "model", "conv")
+    autoencoder.save(os.path.join(model_path, "autoencoder.h5"))
+    encoder.save(os.path.join(model_path, "encoder.h5"))
+    decoder.save(os.path.join(model_path, "decoder.h5"))
+# autoencoder = load_model("model/conv/autoencoder.h5")
+# decoder = load_model("model/conv/decoder.h5")
+# encoder = load_model("model/conv/encoder.h5")
+print("Done")
 
-    autoencoder.save("model/conv/autoencoder.h5")
-    encoder.save("model/conv/encoder.h5")
-    decoder.save("model/conv/decoder.h5")
-# autoencoder = keras.models.load_model("model/conv/autoencoder.h5")
-# decoder = keras.models.load_model("model/conv/decoder.h5")
-# encoder = keras.models.load_model("model/conv/encoder.h5")
+eval_train = autoencoder.evaluate(X_train, X_train)
+eval_validate = autoencoder.evaluate(X_validate, X_validate)
+eval_test = autoencoder.evaluate(X_test, X_test)
+print("Evaluation: Train", eval_train, "Validate",
+      eval_validate, "Test",  eval_test)
 
-encoded_imgs = encoder.predict(X_test)
-decoded_imgs = decoder.predict(encoded_imgs)
+encoded_imgs_train = encoder.predict(X_train)
+encoded_imgs_validate = encoder.predict(X_validate)
+encoded_imgs_test = encoder.predict(X_test)
 
+decoded_imgs = decoder.predict(encoded_imgs_test)
 n = 10  # how many digits we will display
 plt.figure(figsize=(20, 4))
 for i in range(n):
@@ -153,3 +190,53 @@ for i in range(n):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 plt.savefig("conv-autoencoder.png")
+
+#####################################################################
+print("CLASSIFIER")
+#####################################################################
+encoded_imgs_train = encoded_imgs_train.reshape(len(encoded_imgs_train), 128)
+encoded_imgs_validate = encoded_imgs_validate.reshape(
+    len(encoded_imgs_validate), 128)
+encoded_imgs_test = encoded_imgs_test.reshape(len(encoded_imgs_test), 128)
+
+ckpt_loc = os.path.join(CUR_DIR, "ckpts", "classifier.hdf5")
+if(os.path.isfile(ckpt_loc)):
+    print("Loading classifier from directory %s..." % ckpt_loc)
+
+    classifier = load_model(ckpt_loc)
+else:
+    print("Training classifier...")
+    classifier = build_classifier()
+    earlyStopping = EarlyStopping(
+        monitor='val_acc', patience=3, verbose=1, mode='max', min_delta=0.01)
+    mcp_save = ModelCheckpoint(ckpt_loc,
+                               save_best_only=True, verbose=1, monitor='val_acc', mode='max')
+    reduce_lr_loss = ReduceLROnPlateau(
+        monitor='val_acc', factor=0.3, patience=3, verbose=1, mode='max')
+    classifier.fit(encoded_imgs_train, y_train, validation_data=(
+        encoded_imgs_validate, y_validate), batch_size=10, epochs=10, shuffle=True, callbacks=[earlyStopping, mcp_save, reduce_lr_loss])
+print("Done")
+
+eval_train = classifier.evaluate(encoded_imgs_train, y_train)
+eval_validate = classifier.evaluate(encoded_imgs_validate, y_validate)
+eval_test = classifier.evaluate(encoded_imgs_test, y_test)
+print("Evaluation: Train", eval_train, "Validate",
+      eval_validate, "Test",  eval_test)
+
+
+def get_cm(input, y, TRESHOLD):
+    y_pred = classifier.predict(input)
+    y_pred = (y_pred > THRESHOLD)
+
+    y_pred = tf.argmax(y_pred.astype(np.float32), axis=1)
+    y = tf.argmax(y, axis=1)
+    c = tf.keras.backend.eval(y_pred)
+    d = tf.keras.backend.eval(y)
+    # print(c, d)
+    cm = confusion_matrix(c, d)
+    return cm
+
+
+print(get_cm(encoded_imgs_train, y_train, THRESHOLD))
+print(get_cm(encoded_imgs_validate, y_validate, THRESHOLD))
+print(get_cm(encoded_imgs_test, y_test, THRESHOLD))
