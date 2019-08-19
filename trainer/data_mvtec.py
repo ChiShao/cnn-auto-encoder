fb94189c3dd779be4368bceec97244c2230a3b43
daniel-scholz <dscholz@dsBook.localdomain>
2019-08-19 14:48:52 +0200

    fixes data mvtec

--- trainer/data_mvtec.py
+++ trainer/data_mvtec.py
@@ -1,16 +1,12 @@
 import os
 import random
 
-from tensorflow.python.lib.io import file_io
+import tensorflow.io.gfile as gfile
 
 
 def read_image_file_names(dir_path):
     """Returns a list of absolute file paths for relative dir input with all relevant file names."""
-    return [os.path.join(dir_path, p) for p in file_io.list_directory(dir_path)]
-
-
-def get_path_to_data(data_path, USE_CASE, mode):
-    return os.path.join(data_path, USE_CASE, mode)
+    return [os.path.join(dir_path, p) for p in gfile.ListDirectory(dir_path)]
 
 
 def load_data(data_path, USE_CASE):
@@ -18,7 +14,7 @@ def load_data(data_path, USE_CASE):
     normal_file_names = read_image_file_names(normal_path)
     random.shuffle(normal_file_names)
 
-    train_validation_split = (len(normal_file_names) // 10) * 8  # equals .85
+    train_validation_split = (len(normal_file_names) // 10) * 8  # equals .8
     train_file_names, validation_file_names = normal_file_names[
         :train_validation_split], normal_file_names[train_validation_split:]
 
@@ -29,7 +25,7 @@ def load_data(data_path, USE_CASE):
 
     anomaly_test_file_names = anomaly_train_file_names = []
     # iterate of test list dir because anomalies are named dynamically
-    for i, p in enumerate(filter(lambda x: x != "good", file_io.list_directory(test_path))):
+    for i, p in enumerate(filter(lambda x: x != "good", gfile.ListDirectory(test_path))):
         if i == 0:
             anomaly_test_file_names += read_image_file_names(
                 os.path.join(test_path, p))
